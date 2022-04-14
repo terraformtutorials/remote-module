45314b6fc7b6c5050e507db9f74fd43072633abb
terraformtutorials <103517942+terraformtutorials@users.noreply.github.com>
2022-04-14 17:18:24 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,19 @@
 resource "aws_instance" "myec2" {
    ami = "ami-04893cdb768d0f9ee"
    instance_type = "t2.micro"
+   key_name = var.key_name
+   vpc_security_group_ids = var.vpc_security_group_ids
+   subnet_ids = var.subnet_ids
 }
 
+variable "key_name" {
+   default = ""
+}
 
+variable "vpc_security_group_ids" {
+   default = ""
+}
+
+variable "subnet_ids" {
+   default = ""
+}
