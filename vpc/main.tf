fde3b3cda9805060ebc0f4fe40ada958ddb3e3b9
terraformtutorials <103517942+terraformtutorials@users.noreply.github.com>
2022-04-15 01:46:52 +0530

    del

--- vpc/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  instance_tenancy = var.instance_tenancy
-  enable_dns_hostnames = var.enable_dns_hostnames
-  enable_dns_support = var.enable_dns_support
-  tags = var.tags
-}
