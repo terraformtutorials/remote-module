63ebdbeb8b50a845bd5f8923d67857f0ddef5f7f
Abhinindi Rao <abhinindi.r@ebaotech.com>
2022-04-14 17:06:40 +0530

    code update

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,9 +1,3 @@
-provider "aws" {  
-  region = "ap-south-1"
-  access_key = "AKIAYX2PNQFLQ3VX3SZT"
-  secret_key = "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"
-}
-
 resource "aws_instance" "myec2" {
    ami = "ami-04893cdb768d0f9ee"
    instance_type = "t2.micro"
